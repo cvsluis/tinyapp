5c5d5c766dc9d5016e8cd350c1543bc6579f0ae5
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-21 16:41:46 -0700

    feat: add route to display another page

--- express_server.js
+++ express_server.js
@@ -14,9 +14,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
