45b7d4aa1c6f4dadf0538731a4b8e58b11fe04ac
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-23 10:39:18 -0700

    feat: implement updating urls

--- express_server.js
+++ express_server.js
@@ -53,11 +53,18 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
