86973d1ef3d0cc4ee933b0d5fc67c1e96990e346
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-23 14:29:49 -0700

    feat: implement logout client and server logic

--- express_server.js
+++ express_server.js
@@ -28,24 +28,24 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"], 
-    urls: urlDatabase 
+    username: req.cookies["username"],
+    urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"], 
+    username: req.cookies["username"],
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"], 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id] 
+  const templateVars = {
+    username: req.cookies["username"],
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
   };
   res.render("urls_show", templateVars);
 });
@@ -82,6 +82,11 @@ app.post("/urls/:id/delete", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect("/urls");
 });
 
