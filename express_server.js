6ad21fc089f3940ac83419f71940f4225c6282ac
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-22 20:04:28 -0700

    Update server to add link to redirect to longURL

--- express_server.js
+++ express_server.js
@@ -37,6 +37,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
