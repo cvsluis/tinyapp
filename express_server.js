151d3d4fdb363ea27ce556496f96f1e27a6d957f
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-28 15:35:51 -0700

    Update urlDatabase structure

--- express_server.js
+++ express_server.js
@@ -9,8 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "userRandomID"
+  }
 };
 
 const users = {
